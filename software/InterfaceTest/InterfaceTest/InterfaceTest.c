538eded03ed178db6bf190c63fb61d18d125656b
Alex Hornung <a.hornung@ee.ucl.ac.uk>
2010-10-19 13:29:17 +0100

    Break functionality into functions
    * Create if_read, if_write, open_card, close_card, enable_periph and disable_periph.

--- software/InterfaceTest/InterfaceTest/InterfaceTest.c
+++ software/InterfaceTest/InterfaceTest/InterfaceTest.c
@@ -4,20 +4,70 @@
 #include "stdafx.h"
 #include "assert.h"
 
-int _tmain(int argc, _TCHAR* argv[])
+static DWORD if_write(DIME_HANDLE hCard1, DWORD *pdata, DWORD timeout)
+{
+	DWORD tmp_write;
+	DWORD ret;
+
+	tmp_write = 0x0FFFFFF0;
+	assert(((unsigned long)&tmp_write) % 4 == 0);
+	assert(((unsigned long)pdata) % 4 == 0);
+
+	ret = DIME_AddressWriteSingle(hCard1, &tmp_write, NULL, timeout);
+	printf("A) DIME_AddressWriteSingle, ret=%d\n", ret);
+
+	ret = DIME_DataWriteSingle(hCard1, pdata, NULL, timeout);
+	printf("A) DIME_DataWriteSingle, ret=%d\n", ret);
+
+	return ret;
+}
+
+static DWORD if_read(DIME_HANDLE hCard1, DWORD *pdata, DWORD timeout)
+{
+	DWORD tmp_write;
+	DWORD ret;
+
+	tmp_write = 0xFFFFFFFF;
+	assert(((unsigned long)&tmp_write) % 4 == 0);
+	assert(((unsigned long)pdata) % 4 == 0);
+
+	ret = DIME_AddressWriteSingle(hCard1, &tmp_write, NULL, timeout);
+	//printf("B) DIME_AddressWriteSingle, ret=%d\n", ret);
+
+	ret = DIME_DataReadSingle(hCard1, pdata, NULL, timeout);
+	//printf("B) DIME_DataReadSingle, ret=%d\n", ret);
+
+	return ret;
+}
+
+static void enable_periph(DIME_HANDLE hCard1)
+{
+	DWORD data;
+
+	data = 0xFFFFFFFF;
+	if_write(hCard1, &data, 10000);
+}
+
+static void disable_periph(DIME_HANDLE hCard1)
+{
+	DWORD data;
+
+	data = 0x00000000;
+	if_write(hCard1, &data, 10000);
+}
+
+static DWORD open_card(DIME_HANDLE *phCard, LOCATE_HANDLE *phLocate)
 {
 	LOCATE_HANDLE hLocate = NULL;
 	DWORD ErrorNum,NumOfCards;
 	char ErrorString[1000];
 	DIME_HANDLE hCard1;
-	DWORD ret, tmp = 0, tmp_write;
 
 	/* Call the function to locate our BenOne card on the USB bus */
 	if( (hLocate = DIME_LocateCard(dlUSB,mbtTHEBENONE,NULL,dldrDEFAULT,dlDEFAULT)) == NULL)
 	{
 		DIME_GetError(NULL,&ErrorNum,ErrorString);
 		printf("Error %d: %s\n", ErrorNum, ErrorString);
-		getchar();
 		return 1;
 	}
 
@@ -31,41 +81,53 @@ int _tmain(int argc, _TCHAR* argv[])
 	hCard1 = DIME_OpenCard(hLocate,1,dccOPEN_NO_OSCILLATOR_SETUP);
 	if (hCard1 == NULL)
 	{
-		printf("Card Number One failed to open.\n");
+		printf("Card Number one failed to open.\n");
 		DIME_CloseLocate(hLocate);
-		getchar();
 		return 1;
 	}
 
-	printf("Card successfully opened.\n");
+	*phCard = hCard1;
+	*phLocate = hLocate;
+	return 0;
+}
 
-	assert(((unsigned long)&tmp) % 4 == 0);
-	assert(((unsigned long)&tmp_write) % 4 == 0);
+static void close_card(DIME_HANDLE hCard, LOCATE_HANDLE hLocate)
+{
+	DIME_CloseCard(hCard);
+	DIME_CloseLocate(hLocate);
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	LOCATE_HANDLE hLocate;
+	DIME_HANDLE hCard1;
+	DWORD error, tmp;
+	int i;
+
+	error = open_card(&hCard1, &hLocate);
+
+	if (error == 0) {
+		printf("Card successfully opened.\n");
+	} else {
+		printf("Woops, something went wrong opening the card\n");
+		exit(1);
+	}
+		/* NOTREACHED */
+
 	printf("Writing something whenever you press a button\n");
 	getchar();
 
-	tmp_write = 0x0FFFFFF0;
-	ret = DIME_AddressWriteSingle(hCard1, &tmp_write, NULL, 10000);
-	printf("A) DIME_AddressWriteSingle, ret=%d\n", ret);
+	disable_periph(hCard1);
 
-	tmp_write = 0x12345678;
-	ret = DIME_DataWriteSingle(hCard1, &tmp_write, NULL, 10000);
-	printf("A) DIME_DataWriteSingle, ret=%d\n", ret);
+	printf("Continuing on your mark\n");
+	getchar();
 
+	for (i = 0; i < 1096; i++) {
+		error = if_read(hCard1, &tmp, 10000);
+		printf("Read: %#x (error = %d)\n", tmp, error);
+	}
 
-	tmp_write = 0xFFFFFFFF;
-	ret = DIME_AddressWriteSingle(hCard1, &tmp_write, NULL, 10000);
-	printf("B) DIME_AddressWriteSingle, ret=%d\n", ret);
-
-	ret = DIME_DataReadSingle(hCard1, &tmp, NULL, 10000);
-	printf("B) DIME_DataReadSingle, ret=%d\n", ret);
-	printf("Read: %#x\n", tmp);
-
-	//DIME_DataRead(hCard1, ReadData, 128, NULL, NULL, 10000);
-
-
-	DIME_CloseCard(hCard1);
-	DIME_CloseLocate(hLocate);
+	close_card(hCard1, hLocate);
 
 	printf("Done\n");
 	getchar();
